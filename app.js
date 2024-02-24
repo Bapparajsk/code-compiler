e2b51cd5aad0861d5d6be1a12dbdf9cb62975e0a
Bapparaj sk <96433605+Bapparajsk@users.noreply.github.com>
2024-02-24 20:48:22 +0530

    code-compiler-server-upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/run-code', require('./routes/run_code'));
+
+module.exports = app;
---
+++ routes/run_code.js
@@ -0,0 +1,61 @@
+const router = require('express').Router();
+const cuid = require('cuid');
+const { create } = require('../lib/directoryCreate');
+const { deleteFile } = require('../lib/deleteFiles');
+const { javaCodeHandler } = require('../lib/javaCodeHandler');
+const { cppCodeHandler } = require('../lib/cppCodeHandler');
+
+router.post('/', async (req, res) => {
+    try {
+        const { code, input, lang, userName } = req.body;
+        if (!code || !input || !lang || !userName) {
+            return  res.status(400).json({
+                type: "error",
+                error: "isEmpty is inputs"
+            });
+        }
+
+        // Delete folder
+        await deleteFile(`./temp/${userName}`);
+
+        await create('./temp');
+        await create(`./temp/${userName}`);
+        const codeDir = cuid.slug();
+
+        // Compile and execute codes
+        let output;
+        if (lang === "java") {
+            output = await javaCodeHandler(code, input, codeDir, userName, 1);
+        } else if(lang === 'c' || lang === "c++") {
+            output = await cppCodeHandler(code, input, codeDir, userName, 1, 100);
+        } else {
+            return  res.status(400).json({
+                type: "error",
+                error: "invalid language",
+                language: lang
+            });
+        }
+
+        if (output.result === null) {
+            // Handle error
+            res.status(500).json({
+                type: "error",
+                error: output.error
+            });
+            return;
+        }
+        console.log(output);
+        return res.json({
+            result : output.result
+        });
+    } catch (error) {
+        console.error('An error occurred:', error);
+        // Handle error
+        return res.status(500).json({
+            type: "error",
+            error
+        });
+    }
+});
+
+module.exports = router;
